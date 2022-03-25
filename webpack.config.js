06d741122f09e777de9b3e354face9973f3b2d45
PIV <igor@pilipenko.cc>
2022-03-25 15:34:59 +0200

    +

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
+const { merge } = require('webpack-merge');
 const path = require("path")
 
-module.exports = {
-  entry: "./vue-unwrap-element.mjs",
+const commonConfig = {
+  entry: "./vue-unwrap-element.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "vue-unwrap-element.js",
-    library: {
-      name: 'vueUnwrapElement',
-      type: "umd"
-    },
-    globalObject: 'this',
   },
   module: {
     rules: [
@@ -23,3 +18,31 @@ module.exports = {
   mode: "development",
   devtool: 'source-map',
 }
+
+module.exports=[
+
+  // module build
+  merge(commonConfig,{
+    output: {
+      filename: 'vue-unwrap-element.esm.js',
+      library: {
+        type: "module"
+      },
+    },
+    experiments: {
+      outputModule: true
+    }
+  }),
+
+  // common legacy build
+  merge(commonConfig, {
+    output: {
+      filename: 'vue-unwrap-element.js',
+      library: {
+        name: "vueUnwrapElement",
+        type: "umd"
+      },
+      globalObject: 'this',
+    }
+  }),
+];
