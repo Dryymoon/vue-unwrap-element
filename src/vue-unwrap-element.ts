6dce1616e5dfbc6b5f8208127ae65b33a9b2ee92
Pilipenko Igor <igor@pilipenko.cc>
2024-04-25 03:26:13 +0300

    +

--- src/vue-unwrap-element.ts
+++ src/vue-unwrap-element.ts
@@ -1,8 +1,6 @@
 import unwrapElement from 'unwrap-element';
-import type { DirectiveOptions, PluginFunction, VueConstructor } from 'vue';
+import type { DirectiveOptions, PluginFunction } from 'vue';
 import { PluginObject } from 'vue/types/plugin';
-
-let installed = false;
 
 const DESTROY_UNWRAP = Symbol('destroyUnwrap');
 
@@ -10,45 +8,52 @@ interface HTMLElementWithUnwrappedData extends HTMLElement {
   [DESTROY_UNWRAP]?: ReturnType<typeof unwrapElement>;
 }
 
-const doUnWrap = (Vue: VueConstructor, el: HTMLElementWithUnwrappedData) =>
+const doUnWrap = (
+  el: HTMLElementWithUnwrappedData,
+  nextTick: () => Promise<void> | undefined,
+) =>
   unwrapElement(el, {
     beforeRestoreScroll: async () => {
-      await Vue.nextTick();
-      await Vue.nextTick();
+      if (nextTick) {
+        await nextTick();
+        await nextTick();
+      }
     },
   });
 
+const unwrapElDirective: DirectiveOptions = {
+  inserted(el: HTMLElementWithUnwrappedData, { value = true }, vNode) {
+    if (value) {
+      el[DESTROY_UNWRAP] = doUnWrap(el, () => vNode?.context?.$nextTick?.());
+    }
+  },
+  update(el: HTMLElementWithUnwrappedData, { value }, vNode) {
+    if (value && !el[DESTROY_UNWRAP]) {
+      el[DESTROY_UNWRAP] = doUnWrap(el, () => vNode?.context?.$nextTick?.());
+    } else if (!value && el[DESTROY_UNWRAP]) {
+      el[DESTROY_UNWRAP]();
+      el[DESTROY_UNWRAP] = undefined;
+      delete el[DESTROY_UNWRAP];
+    }
+  },
+  unbind(el: HTMLElementWithUnwrappedData) {
+    if (el[DESTROY_UNWRAP]) {
+      el[DESTROY_UNWRAP]();
+      el[DESTROY_UNWRAP] = undefined;
+      delete el[DESTROY_UNWRAP];
+    }
+  },
+};
+
 export const install: PluginFunction<unknown> = Vue => {
-  if (installed) return;
-  installed = true;
-
-  const unwrapElDirective: DirectiveOptions = {
-    inserted(el: HTMLElementWithUnwrappedData, { value = true }) {
-      if (value) {
-        el[DESTROY_UNWRAP] = doUnWrap(Vue, el);
-      }
-    },
-    update(el: HTMLElementWithUnwrappedData, { value }) {
-      if (value && !el[DESTROY_UNWRAP]) {
-        el[DESTROY_UNWRAP] = doUnWrap(Vue, el);
-      } else if (!value && el[DESTROY_UNWRAP]) {
-        el[DESTROY_UNWRAP]();
-        el[DESTROY_UNWRAP] = undefined;
-        delete el[DESTROY_UNWRAP];
-      }
-    },
-    unbind(el: HTMLElementWithUnwrappedData) {
-      if (el[DESTROY_UNWRAP]) {
-        el[DESTROY_UNWRAP]();
-        el[DESTROY_UNWRAP] = undefined;
-        delete el[DESTROY_UNWRAP];
-      }
-    },
-  };
-
   Vue.directive('unwrap-element', unwrapElDirective);
 };
 
-const plugin: PluginObject<unknown> = { install };
+const plugin: PluginObject<unknown> = {
+  install,
+  directive: {
+    'unwrap-element': unwrapElDirective,
+  },
+};
 
 export default plugin;
